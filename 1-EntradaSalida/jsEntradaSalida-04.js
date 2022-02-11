edbf39780546a33b938db0ac69fd785499e1072d
Valentina Montenegro <valentinamontenegro@MBPdeValentina.fibertel.com.ar>
2022-02-11 01:03:47 -0300

    Ok

--- 1-EntradaSalida/jsEntradaSalida-04.js
+++ 1-EntradaSalida/jsEntradaSalida-04.js
@@ -4,6 +4,13 @@ y lo muestro por 'ID' al presionar el botón 'mostrar'
 */
 function mostrar()
 {
-	alert("ok");
+	let nombre
+
+	//guardo en la variable nombre el texto que escribio el usuario en la ventana prompt
+	nombre =prompt("Ingrese su nombre")
+	
+	//Copio en nombre lo que tengo guardado en la caja de txt de html
+	document.getElementById("txtIdNombre").value = nombre
+	
 }
 
--- 1-EntradaSalida/jsEntradaSalida-06.js
+++ 1-EntradaSalida/jsEntradaSalida-06.js
@@ -4,6 +4,15 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
-	alert("ok");
+	let numero1
+	let numero2
+
+	numero1 = document.getElementById("txtIdNumeroUno").value 
+
+	numero2 = document.getElementById("txtIdNumeroDos").value
+	
+	suma = numero1 + numero2
+
+	alert("La suma es " + suma);
 }
 
