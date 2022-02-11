7f8b37b5c339193b222ad41759f430965ba2d82c
Valentina Montenegro <valentinamontenegro@MBPdeValentina.fibertel.com.ar>
2022-02-11 18:57:50 -0300

    OKK

--- 8-TPs/01-jsFerreteFacturacion.js
+++ 8-TPs/01-jsFerreteFacturacion.js
@@ -5,13 +5,67 @@ C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
-	
+let producto1
+let producto2
+let producto3
+let suma	
+
+producto1 = document.getElementById("txtIdPrecioUno").value 
+producto1 = parseFloat(producto1)
+
+producto2 = document.getElementById("txtIdPrecioDos").value 
+producto2 = parseFloat(producto2)
+
+producto3 = document.getElementById("txtIdPrecioTres").value 
+producto3 = parseFloat(producto3)
+
+suma = producto1 + producto2 + producto3
+
+alert(suma)
 }
 function Promedio () 
 {
-	
-}
+    let producto1
+    let producto2
+    let producto3
+    let suma
+    let promedio	
+    
+    producto1 = document.getElementById("txtIdPrecioUno").value 
+    producto1 = parseFloat(producto1)
+    
+    producto2 = document.getElementById("txtIdPrecioDos").value 
+    producto2 = parseFloat(producto2)
+    
+    producto3 = document.getElementById("txtIdPrecioTres").value 
+    producto3 = parseFloat(producto3)
+    
+    suma = producto1 + producto2 + producto3 
+    promedio = suma / 3
+    
+    alert(promedio)
+    }
+
 function PrecioFinal () 
 {
-	
-}
+    let producto1
+    let producto2
+    let producto3
+    let suma
+    let suma21
+    
+    producto1 = document.getElementById("txtIdPrecioUno").value 
+    producto1 = parseFloat(producto1)
+    
+    producto2 = document.getElementById("txtIdPrecioDos").value 
+    producto2 = parseFloat(producto2)
+    
+    producto3 = document.getElementById("txtIdPrecioTres").value 
+    producto3 = parseFloat(producto3)
+    
+    suma = producto1 + producto2 + producto3
+
+    suma21 = suma + suma * 21 / 100
+    
+    alert(suma21)
+    }
--- 8-TPs/02-jsFerreteConstruccion.js
+++ 8-TPs/02-jsFerreteConstruccion.js
@@ -6,13 +6,66 @@ C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de c
 
 function Rectangulo () 
 {
+let largo
+let ancho
+let rectangulo
+let perimetro
+
+largo = document.getElementById("txtIdLargo").value 
+largo = parseFloat(largo)
+
+ancho = document.getElementById("txtIdAncho").value 
+ancho = parseFloat(ancho)
+
+rectangulo = (largo + ancho) * 2
+
+perimetro = rectangulo * 3
+
+alert("Se deben comprar " + perimetro + " metros de alambre.")
 
 }
 function Circulo () 
 {
-	
+let radio
+let circunferencia
+let alambrar
+    
+radio = document.getElementById("txtIdRadio").value 
+radio = parseFloat(radio)
+
+circunferencia = radio * 2 * 3.14
+
+alambrar = circunferencia * 3
+
+alert("Se deben comprar  " + alambrar + " metros de alambre.")
+
+
 }
 function Materiales () 
 {
-	
+    let largo
+    let ancho
+    let superficie
+    let cemento
+    let cal 
+    
+    largo = document.getElementById("txtIdLargo").value 
+    largo = parseFloat(largo)
+    
+    ancho = document.getElementById("txtIdAncho").value 
+    ancho = parseFloat(ancho)
+    
+    superficie = largo * ancho 
+
+    cemento = superficie * 2
+    cal = superficie * 3
+
+    alert("Para el contrapiso de "+ superficie + "m cuadrados necesito comprar "+ cemento + " bolsas de cemento y "+cal+" bolsas de cal.")
+
+    
+
+
+
+
+
 }
