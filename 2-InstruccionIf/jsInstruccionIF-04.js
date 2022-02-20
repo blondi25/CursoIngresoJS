462126f1ca8f6ebd113b277bbc1796f39a12f787
Valentina Montenegro <valentinamontenegro@MBPdeValentina.fibertel.com.ar>
2022-02-20 10:23:58 -0300

    Buenisimo

--- 2-InstruccionIf/jsInstruccionIF-04.js
+++ 2-InstruccionIf/jsInstruccionIF-04.js
@@ -1,7 +1,13 @@
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
+	let edad;
 
+	edad = parseInt(document.getElementById("txtIdEdad").value);
+	
+	if (edad >= 13 && edad <= 17) {
 
-}//FIN DE LA FUNCIÓN
+		alert("Eres adolescente");
+
+	}
+
+}
